0d8c2e7728301294313036e3aeb83165594a7ffd
chengkev <chengkev2000@gmail.com>
2023-01-27 11:02:07 -0400

    fix: standarize error on invalid session

--- src/middlewares/verifyUser.middleware.js
+++ src/middlewares/verifyUser.middleware.js
@@ -5,9 +5,9 @@ const { controllerWrapper, getAuthSession } = require("../utils/common")
 const verifyUser = (get = true) => controllerWrapper(async (req, res, next) => {
     const sessionId = getAuthSession(req.headers.authorization)
     const session = await Session.findByPk(sessionId)
-    if(!session) throw HttpStatusError.notFound("Session not found")
+    if(!session) throw HttpStatusError.unauthorize("Invalid Session")
     const user = await User.findByPk(session.userId, {include: Subsidiary})
-    if(!user) throw HttpStatusError.notFound("User not found")
+    if(!user) throw HttpStatusError.unauthorize("Invalid Session")
     if(get) req.user = user
     next()
 })
